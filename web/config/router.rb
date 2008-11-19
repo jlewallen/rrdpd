ea28b069c6227fc75ce2375ea5b257a4e6e8b09c
Jacob Lewallen <jlewallen@gmail.com>
2008-11-18 23:42:26 -0800

    Adding default route for welcome controller.

--- web/config/router.rb
+++ web/config/router.rb
@@ -42,5 +42,5 @@ Merb::Router.prepare do
   default_routes
   
   # Change this for your home page to be available at /
-  # match('/').to(:controller => 'whatever', :action =>'index')
+  match('/').to(:controller => 'welcome', :action => 'index')
 end
