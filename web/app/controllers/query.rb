83e1a4e3405bfdbe07fd19466f04a765231a856a
Jacob Lewallen <jlewallen@gmail.com>
2008-11-23 00:09:55 -0800

    Cleaning up routes, nothing extraneous.

--- web/app/controllers/query.rb
+++ web/app/controllers/query.rb
@@ -11,18 +11,6 @@ class Query < Application
     display @query
   end
 
-  def item(category, name, source, counter)
-    @query = DataManager.find_item(category, name).browser(source, counter.to_sym)
-    @query.instance_eval "def to_html; '<pre>' + JsonPrinter.render(JSON.parse(to_json)) + '</pre>'; end"
-    display @query
-  end
-
-  def database(name)
-    @query = DataManager.find_database_by_name(name)
-    @query.instance_eval "def to_html; '<pre>' + JsonPrinter.render(JSON.parse(to_json)) + '</pre>'; end"
-    display @query
-  end
-
   def graph(category, name, source, counter, starting)
     @query = DataManager.find_item(category, name).browser(source, counter.to_sym, { :starting => starting })
     @query.instance_eval "def to_html; '<pre>' + JsonPrinter.render(JSON.parse(to_json)) + '</pre>'; end"
